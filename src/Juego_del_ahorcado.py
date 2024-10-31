13f8770e62a73d84bd69ea7b48d452ca9f6554d7
David Santis <dssoto93@gmail.com>
2024-10-30 22:30:22 -0300

    Escribiendo chequeo de condicion ganadora

--- src/Juego_del_ahorcado.py
+++ src/Juego_del_ahorcado.py
@@ -10,7 +10,7 @@ class JuegoDelAhorcado:
         self.ventana.configure(bg='yellow')
         self.intentos_correctos = set()
         self.intentos_incorrectos = set()
-        self.palabra_secreta  = self.elegir_palabra_secreta().upper().strip()
+        self.palabra_secreta = self.elegir_palabra_secreta().upper().strip()
         #self.palabra_secreta = unicodedata.normalize('NFKD', self.palabra_secreta).encode('ASCII', 'ignore')
         
 
@@ -23,7 +23,7 @@ class JuegoDelAhorcado:
 
     def elegir_palabra_secreta(self):
 
-        filename = 'palabras.txt'
+        filename = 'palabras_facil.txt'
         
         with open(filename) as archivo_objeto:
             lineas = archivo_objeto.readlines()
@@ -62,32 +62,36 @@ class JuegoDelAhorcado:
     def adivinar_letra(self,letra):
         if letra in self.palabra_secreta and letra not in self.intentos_correctos:
             self.intentos_correctos.add(letra)
+            print(self.intentos_correctos)
         elif letra not in self.intentos_incorrectos:
             self.intentos_incorrectos.add(letra)
             self.num_intentos_incorrectos -= 1
             #self.actualizar_dibujo_ahorcado ()
 
         self.actualizar_mostrar_palabra()
-        #self.chequear_fin_juego ()
+        self.chequear_condicion_ganadora()
     
     def actualizar_mostrar_palabra(self):
         mostrar_palabra = " ".join([letra if letra in self.intentos_correctos else "_" for letra in self.palabra_secreta])
         self.mostrar_palabra.config(text=mostrar_palabra)        
-       
-
-
-
-
-
-
-
     
-        
+    def chequear_condicion_ganadora(self): 
+        if set(self.palabra_secreta).issubset(self.intentos_correctos):
+            self.mostrar_mensajes('FELICIDADES!, HA GANADO EL JUEGO')
+        elif self.num_intentos_incorrectos == 0:
+            self.mostrar_mensajes('LO SENTIMOS!, HA CONSUMIDO TODOS LOS INTENTOS')
+
+
+    def mostrar_mensajes(self,mensaje):
+
+        self.ventana_botones_alfabeto.pack_forget() # quitar botones del alfabero para mostrar mensajes
+
+        self.mensajes_fin_juego = Label(self.ventana,text=mensaje,fg= 'red',font=('Arial',30))
+        self.mensajes_fin_juego.pack(pady=15)       
               
 
 
-    
-    
+
 
 
 def main():
